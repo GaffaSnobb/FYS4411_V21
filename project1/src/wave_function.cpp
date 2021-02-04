3b383dc519a2bbda4b561b2ecf788c34902d482d
Jon Dahl <jonkd@uio.no>
2021-02-04 20:07:00 +0100

    Remove unused code.

--- project1/src/wave_function.cpp
+++ project1/src/wave_function.cpp
@@ -3,16 +3,6 @@
 
 double wave_function(double x, double y, double z, double alpha, double beta)
 {
-    // double result = 1;
-    // for (int _ = 0; _ < n; _++)
-    // {
-    //     result *= std::exp(-alpha*(x*x + y*y + beta*z*z));
-    // }
-
-    // Interaction term:
-    // Double loop for f(...)
-
-    // return result;
     return std::exp(-alpha*(x*x + y*y + beta*z*z));
 }
 
