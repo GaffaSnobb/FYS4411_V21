0ca63062394d06f9dba88a236d2f1e0cec08943e
Jon Dahl <jonkd@uio.no>
2021-02-04 14:18:59 +0100

    Change data file directory.

--- project1/src/read_from_file.py
+++ project1/src/read_from_file.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def read_from_file():
-    alpha, var, exp = np.loadtxt(fname="outfile.txt", skiprows=1, unpack=True)
+    alpha, var, exp = np.loadtxt(fname="generated_data/output.txt", skiprows=1, unpack=True)
     plt.plot(alpha, exp)
+    plt.xlabel("alpha")
+    plt.ylabel("energy")
     plt.show()
 
 
