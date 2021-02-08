7d73e6f1ca395d6cdf4112cfd73400606b547d5c
alidafh <alidafh@gmail.com>
2021-02-08 18:31:33 +0100

    plotting the difference between results from brute force and importance sampling

--- project1/src/read_from_file.py
+++ project1/src/read_from_file.py
@@ -12,12 +12,20 @@ def read_from_file_v2():
     alpha_impor, var_impor, exp_impor = np.loadtxt(fname="generated_data/output_importance.txt", skiprows=1, unpack=True)
     alpha_brute, var_brute, exp_brute = np.loadtxt(fname="generated_data/output_bruteforce.txt", skiprows=1, unpack=True)
 
+    plt.figure()
     plt.plot(alpha_impor, exp_impor, label="importance")
     plt.plot(alpha_brute, exp_brute, label="brute force")
     plt.xlabel("alpha")
     plt.ylabel("energy")
     plt.legend()
     plt.show()
+
+    plt.figure()
+    plt.plot(alpha_impor, exp_impor-exp_brute, label="difference")
+    plt.xlabel("alpha")
+    plt.ylabel("energy")
+    plt.show()
+
 
 
 if __name__ == "__main__":
