f46f3153b9313bfc8dbab8a9663de21c9a9ae43a
Jon Dahl <jonkd@uio.no>
2021-02-10 16:57:00 +0100

    Add generalized wave function exponent calculation. Tested three different ways. Implemented the fastest one.

--- project1/src/wave_function.cpp
+++ project1/src/wave_function.cpp
@@ -11,4 +11,71 @@ double wave_function_exponent(double x, double y, double z, double alpha, double
     return -alpha*(x*x + y*y + beta*z*z);
 }
 
+double wave_function_exponent(arma::Col<double> pos, double alpha, double beta, int n_dims)
+{   /*
+    Parameters
+    ----------
+    pos : arma::Col<double>
+        Vector of x, y, z position of a particle.
+
+    alpha : double
+        Variational parameter.
+
+    beta : double
+        ??? parameter.
+
+    n_dims : int
+        The number of spatial dimensions and the length of pos vector.
+
+    Returns
+    -------
+    result : double
+        The resulting wave function exponent.
+    */
+    double result = 0;
+
+    for (int dim = 0; dim < n_dims; dim++)
+    {   
+        if (dim == 2)
+        {
+            result += pos(dim)*pos(dim)*beta;
+        }
+        else
+        {
+            result += pos(dim)*pos(dim);
+        }
+    }
+    return -alpha*result;
+
+    // double result = pos(0)*pos(0);  // At least one spatial dimension.
+    // if (n_dims == 1)
+    // {
+    //     return -alpha*result;
+    // }
+    // else if (n_dims == 2)
+    // {   
+    //     result += pos(1)*pos(1);
+    //     return -alpha*result;
+    // }
+    // else
+    // {   
+    //     result += pos(1)*pos(1) + pos(2)*pos(2);
+    //     return -alpha*result;
+    // }
+
+    // switch (n_dims)
+    // {
+    //     case 1:
+    //         return -alpha*result;
+        
+    //     case 2:
+    //         result += pos(1)*pos(1);
+    //         return -alpha*result;
+        
+    //     default:
+    //         result += pos(1)*pos(1) + pos(2)*pos(2);
+    //         return -alpha*result;
+    // }
+}
+
 #endif
