da7710e9b6811a6d61119620da4b1f6124a44b15
alidafh <alidafh@gmail.com>
2021-02-15 15:00:22 +0100

    temporary fix to read data in tracker_* file

--- project1/src/read_from_file.py
+++ project1/src/read_from_file.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read_from_file():
-    alpha, var, exp = np.loadtxt(fname="generated_data/output.txt", skiprows=1, unpack=True)
-    plt.plot(alpha, exp)
-    plt.xlabel("alpha")
-    plt.ylabel("energy")
-    plt.show()
+def read_from_file(filename=""):
+    alpha, var, exp = np.loadtxt(fname=filename, skiprows=1, unpack=True)
+    return alpha, var, exp
 
 def read_from_file_v2():
+    """
+    Not in use
+    """
     alpha_impor, var_impor, exp_impor = np.loadtxt(fname="generated_data/output_importance.txt", skiprows=1, unpack=True)
     alpha_brute, var_brute, exp_brute = np.loadtxt(fname="generated_data/output_bruteforce.txt", skiprows=1, unpack=True)
 
@@ -20,14 +20,11 @@ def read_from_file_v2():
     plt.legend()
     plt.show()
 
-    # plt.figure()
-    # plt.plot(alpha_impor, exp_impor-exp_brute, label="difference")
-    # plt.xlabel("alpha")
-    # plt.ylabel("energy")
-    # plt.show()
-
-
+def read_tracker_dt_file(filename=""):
+    dt, t_i, t_b, n_i, n_b  = np.loadtxt(fname=filename, skiprows=19, unpack=True,  delimiter="|")
+    return dt, t_i, t_b, n_i, n_b
 
 if __name__ == "__main__":
     #read_from_file()
-    read_from_file_v2()
+    #read_from_file_v2()
+    read_tracker_dt_file("tracker_dt_importance.txt")
