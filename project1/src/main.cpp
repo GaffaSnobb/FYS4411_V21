9b843f08c37fd7974d74049cee747b0965824a03
alidafh <alidafh@gmail.com>
2021-02-24 11:07:57 +0100

    small comment

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    const int n_dims = 3;
+    const int n_dims = 3;           // Number of dimentions
     const int n_variations = 100;   // Number of variational parameters.
     const int gd_iterations = 100;  // Max. gradient descent iterations.
     // const double time_step = 0.4;   // Time step for importance. TODO: Make this an input to ImportanceSampling.
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
     std::chrono::steady_clock::time_point t2;
     std::chrono::duration<double> comp_time;
 
-    // Importance: 
+    // Importance:
     t1 = std::chrono::steady_clock::now();
-    
+
     ImportanceSampling system_1(n_dims, n_variations);
     system_1.solve();
     system_1.write_to_file("generated_data/output_importance.txt");
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
     // Brute:
     t1 = std::chrono::steady_clock::now();
-    
+
     BruteForce system_2(n_dims, n_variations);
     system_2.solve();
     system_2.write_to_file("generated_data/output_brute_force.txt");
@@ -33,10 +33,10 @@ int main(int argc, char *argv[])
     t2 = std::chrono::steady_clock::now();
     comp_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
     std::cout << "\ntotal time: " << comp_time.count() << "s" << std::endl;
-    
+
     // GD:
     t1 = std::chrono::steady_clock::now();
-    
+
     GradientDescent system_3(n_dims, gd_iterations);
     system_3.solve();
     system_3.write_to_file("generated_data/output_gradient_descent.txt");
