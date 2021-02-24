f0b19382787826109d8529d883cd3c1431d56013
alidafh <alidafh@gmail.com>
2021-02-24 11:07:40 +0100

    deleted deprecated plot function

--- project1/src/visualize.py
+++ project1/src/visualize.py
@@ -9,46 +9,44 @@ def brute_and_importance(fname_brute, fname_importance):
     fig = plt.figure()
     plt.plot(alpha_importance, exp_importance, color="k", label="importance")
     plt.plot(alpha_brute, exp_brute, color="tab:blue", label="brute force")
-    plt.xlabel("alpha")
-    plt.ylabel("energy")
-    plt.legend()
-    fig.savefig("../fig/compare_brute_importance.png")
-    plt.show()
-
-def importance_time_step(path):
-    dt, t_i, t_b, n_i, n_b  = read_tracker_dt_file("tracker_dt_importance.txt")
-
-    fig = plt.figure()
-    for i in range(len(dt)):
-        filename = f"{path}/output_importance_{dt[i]}.txt"
-        alpha, var, exp = read_from_file(filename)
-        plt.plot(alpha, exp, label=f"time_step = {dt[i]}")
+    #plt.xlabel("alpha")
+    #plt.ylabel("energy")
     plt.xlabel(r"$ \alpha $")
     plt.ylabel(r"$\langle E \rangle $")
     plt.legend()
+    fig.savefig("../fig/compare_brute_importance.png")
     plt.show()
-
-    """
-    fig = plt.figure()
-    plt.plot(dt, n_i, label="accepted importance")
-    plt.plot(dt, n_b, label="accepted brute force")
-    plt.xlabel(r"$\Delta t$")
-    plt.ylabel("number")
-    plt.legend()
-    plt.show()
-    """
 
 def gradient_descent(fname):
     alpha, var, exp = read_from_file(fname)
     plt.plot(alpha, exp, ".")
     plt.show()
 
+
+def plot_all(fname_brute, fname_importance, fname_gradient_descent):
+    """
+    temporary plot function
+    """
+    alpha_brute, var_brute, exp_brute = read_from_file(fname_brute)
+    alpha_impor, var_impor, exp_impor = read_from_file(fname_importance)
+    alpha_gradi, var_gradi, exp_gradi = read_from_file(fname_gradient_descent)
+
+    fig = plt.figure()
+    plt.plot(alpha_impor, exp_impor, color="k", label="importance")
+    plt.plot(alpha_brute, exp_brute, color="tab:blue", label="brute force")
+    plt.plot(alpha_gradi, exp_gradi, ".", color="tab:green", label = "gradient descent")
+    plt.xlabel(r"$ \alpha $")
+    plt.ylabel(r"$\langle E \rangle $")
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
     path = "generated_data"
     fname_brute_force = f"{path}/output_brute_force.txt"
     fname_importance = f"{path}/output_importance.txt"
     fname_gradient_descent = f"{path}/output_gradient_descent.txt"
-    # brute_and_importance(fname_brute=fname_brute_force, fname_importance=fname_importance)
+    brute_and_importance(fname_brute=fname_brute_force, fname_importance=fname_importance)
     gradient_descent(fname_gradient_descent)
 
-    #importance_time_step(path)
+    #plot_all(fname_brute_force, fname_importance, fname_gradient_descent)
