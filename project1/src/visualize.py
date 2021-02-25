a079aa94ed72399fb8cffce1b3a1b8f5039e9d78
alidafh <alidafh@gmail.com>
2021-02-25 12:27:24 +0100

    plot with variance included

--- project1/src/visualize.py
+++ project1/src/visualize.py
@@ -29,12 +29,14 @@ def local_energy_alpha(fname, type):
     alpha, var_energy, exp_energy = read_from_file(fname)
 
     fig = plt.figure()
+    plt.grid()
     plt.title(type)
     plt.plot(alpha, exp_energy, color="k", label="Expected local energy")
-    #plt.fill_between(alpha, exp_energy - var_energy, exp_energy + var_energy, color="k", alpha=0.2)
+    plt.fill_between(alpha, exp_energy - np.sqrt(var_energy), exp_energy + np.sqrt(var_energy), color="k", alpha=0.2, label="std")
     plt.xlabel(r"$ \alpha $")
     plt.ylabel(r"E/N")
     plt.legend()
+    fig.savefig(f"../fig/VMC_{type}_variance.png")
     plt.show()
 
 
@@ -43,10 +45,10 @@ if __name__ == "__main__":
     fname_brute_force = f"{path}/output_brute_force.txt"
     fname_importance = f"{path}/output_importance.txt"
     fname_gradient_descent = f"{path}/output_gradient_descent.txt"
-    brute_and_importance(fname_brute=fname_brute_force, fname_importance=fname_importance)
-    gradient_descent(fname_gradient_descent)
+    #brute_and_importance(fname_brute=fname_brute_force, fname_importance=fname_importance)
+    #gradient_descent(fname_gradient_descent)
 
-    #f_importance = f"{path}/output_importance_particles.txt"
-    #f_brute_force = f"{path}/output_brute_force_particles.txt"
-    #local_energy_alpha(f_brute_force, "brute force")
-    #local_energy_alpha(f_importance, "importance")
+    f_importance = f"{path}/output_importance_particles.txt"
+    f_brute_force = f"{path}/output_brute_force_particles.txt"
+    local_energy_alpha(f_brute_force, "brute_force")
+    local_energy_alpha(f_importance, "importance")
