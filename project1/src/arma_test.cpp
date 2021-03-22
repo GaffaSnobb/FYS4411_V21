68de901110565b4a7d0165ce8cab62e80cdada60
Jon Dahl <jon@dira.no>
2021-03-22 19:05:01 +0100

    RNG testing.

--- project1/src/arma_test.cpp
+++ project1/src/arma_test.cpp
@@ -1,10 +1,45 @@
 #include <armadillo>
+#include <random>
+#include "omp.h"
+
+void not_thread_safe()
+{
+    arma::Mat<int> M(20, 2);
+    M.zeros();
+    std::mt19937 engine(1337);
+    std::uniform_int_distribution<int> distribution(1, 10);
+    omp_set_num_threads(2);
+    omp_set_dynamic(0);
+
+    #pragma omp parallel
+    {
+        std::cout << "\n" << omp_get_thread_num() << std::endl;
+        for (int i = 0; i < 20; i++)
+        {
+            M(i, omp_get_thread_num()) = distribution(engine);
+        }
+    }
+    M.print();
+}
 
 int main()
 {   
-    arma::vec M(3);
+    arma::Mat<int> M(20, 3);
     M.zeros();
-    double a = arma::norm(M, 2);
-    std::cout << a << std::endl;
+    std::mt19937 engine;
+    std::uniform_int_distribution<int> distribution(1, 10);
+    omp_set_num_threads(3);
+    omp_set_dynamic(0);
+
+    #pragma omp parallel private(engine)
+    {   
+        engine.seed(omp_get_thread_num());
+        std::cout << "\n" << omp_get_thread_num() << std::endl;
+        for (int i = 0; i < 20; i++)
+        {
+            M(i, omp_get_thread_num()) = distribution(engine);
+        }
+    }
+    M.print();
     return 0;
 }
