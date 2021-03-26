b92cd647841fec3a3d759a11350c84f4c1fc4f92
Jon Dahl <jon@dira.no>
2021-03-26 18:49:46 +0100

    Begin plotting for task 1b.

--- project1/src/visualize.py
+++ project1/src/visualize.py
@@ -1,6 +1,8 @@
+import os
+from matplotlib import interactive
 import numpy as np
 import matplotlib.pyplot as plt
-from read_from_file import read_from_file, read_energy_from_file
+from read_from_file import read_from_file, read_energy_from_file, read_all_files
 
 def brute_and_importance(fname_brute, fname_importance):
     alpha_brute, var_brute, exp_brute, n_brute = read_from_file(fname_brute)
@@ -43,7 +45,7 @@ def local_energy_alpha(fname, method):
     """
     temporary plot function
     """
-    alpha, var_energy, exp_energy = read_from_file(fname)
+    alpha, var_energy, exp_energy, time = read_from_file(fname)
 
     #exp_energy /= n_particles
     #var_energy /= n_particles
@@ -89,19 +91,77 @@ def onebody(fname):
     plt.ylabel("scaled counts")
     plt.show()
 
+
+def task_1b():
+    brute = read_all_files(filter_method="brute", filter_data_type="particles")
+    importance = read_all_files(filter_method="importance", filter_data_type="particles")
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(8, 6))
+    ax = ax.ravel()
+    # ax[0].set_xlabel(r"$\alpha$")
+    # ax[0].set_ylabel("Energy per particle")
+    fig.text(x=0.5, y=0.01, s=r"$\alpha$", fontsize=15)
+    fig.text(x=0.005, y=0.35, s=r"Energy per particle", fontsize=15, rotation="vertical")
+
+    ax[0].plot(
+        brute[0].data[:, 0],
+        brute[0].data[:, 2],
+        label = f"Brute"
+    )
+    ax[0].plot(
+        importance[0].data[:, 0],
+        importance[0].data[:, 2],
+        label = f"Importance"
+    )
+
+
+    # Place legend in ax[1].
+    handles, labels = ax[0].get_legend_handles_labels()
+    ax[1].legend(handles, labels, loc='center')
+    ax[1].axis("off")
+
+    ax[2].plot(
+        brute[1].data[:, 0],
+        brute[1].data[:, 2],
+        label = f"Brute\nn particles: {brute[1].n_particles}"
+    )
+    ax[2].plot(
+        importance[1].data[:, 0],
+        importance[1].data[:, 2],
+        label = f"Importance\nn particles: {importance[1].n_particles}"
+    )
+    ax[3].plot(
+        brute[2].data[:, 0],
+        brute[2].data[:, 2],
+        label = f"Brute\nn particles: {brute[2].n_particles}"
+    )
+
+    for i in range(len(ax)):
+        ax[i].tick_params(labelsize=13)
+
+    fig.tight_layout(pad=2)
+    plt.show()
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    path = "generated_data"
-    fname_brute_force = f"{path}/output_brute_force.txt"
-    fname_importance = f"{path}/output_importance.txt"
-    fname_gradient_descent = f"{path}/output_gradient_descent.txt"
+    # path = "generated_data"
+    # fname_brute_force = f"{path}/output_brute_force.txt"
+    # fname_importance = f"{path}/output_importance.txt"
+    # fname_gradient_descent = f"{path}/output_gradient_descent.txt"
     #brute_and_importance(fname_brute=fname_brute_force, fname_importance=fname_importance)
     #gradient_descent(fname_gradient_descent)
 
-    f_importance = f"{path}/output_importance_particles.txt"
-    f_brute_force = f"{path}/output_brute_force_particles.txt"
-    f_brute_force_onebody = f"{path}/output_brute_force_onebody_density.txt"
+    # f_importance = f"{path}/output_importance_particles.txt"
+    # f_brute_force = f"{path}/output_brute_force_particles.txt"
+    # f_brute_force_onebody = f"{path}/output_brute_force_onebody_density.txt"
     # local_energy_alpha(f_brute_force, "brute_force")
     # local_energy_alpha(f_importance, "importance")
     # local_energy_alpha(f"{path}/output_gradient_descent_particles.txt", "GD")
     # tmp_gd()
-    onebody(f_brute_force_onebody)
+    # onebody(f_brute_force_onebody)
+    task_1b()
