59b11f1e0a84514a6fccd43c40dab55e65213c0a
Jon Dahl <jon@dira.no>
2021-03-26 18:49:17 +0100

    Add function that reads all files in generated_data/. Calculation parameters are read from data file name and stored as attributes in a simple container class.

--- project1/src/read_from_file.py
+++ project1/src/read_from_file.py
@@ -1,5 +1,154 @@
+import os
 import numpy as np
-import matplotlib.pyplot as plt
+
+class Container:
+    def __init__(
+        self,
+        data,
+        method,
+        n_particles,
+        n_dims,
+        n_mc_cycles,
+        step_size,
+        numerical,
+        interaction,
+        data_type,
+        fname
+    ):
+        """
+        Parameters
+        ----------
+
+        data : numpy.ndarray
+            Array with data from data file.
+        
+        method : string
+            'brute', 'importance', or 'gradient'.
+
+        n_particles : integer
+            Total number of particles.
+
+        n_dims : integer
+            Number of spatial dimensions.
+
+        n_mc_cycles : integer
+            Number of Monte Carlo cycles.
+
+        step_size : float
+            Either importance time step or brute force step size.
+
+        numerical : boolean
+            True if numerical differentiation, False if analytical.
+
+        interaction : boolean
+            Interaction term on / off.
+
+        data_type : string
+            'particles', 'onebody', or 'energies'.
+
+        fname : string
+            Original file name.
+        """
+        self.data = data
+        self.method = method
+        self.n_particles = n_particles
+        self.n_dims = n_dims
+        self.n_mc_cycles = n_mc_cycles
+        self.step_size = step_size
+        self.numerical = numerical
+        self.interaction = interaction
+        self.data_type = data_type
+        self.fname = fname
+
+
+def read_all_files(filter_method=None, filter_data_type=None):
+    """
+    Read all text files in generated_data/ and store all relevant data
+    in 'Container' objects. See Container docstring for details.
+
+    Parameters
+    ----------
+    filter_method : NoneType, string
+        Filter for only reading certain data files. Valid filters are
+        'brute', 'importance', 'gradient'. If None, no filter is
+        applied.
+
+    filter_data_type : NoneType, string
+        Filter for only reading certain data types. Valid filters are
+        'particle', 'onebody', 'energies'. If None, no filter is
+        applied.
+
+    Returns
+    -------
+    data_list : list
+        List of Container objects.
+    """
+    fnames = os.listdir("generated_data/")
+    data_list = []
+
+    for i in range(len(fnames)):
+        fname = fnames[i].split("_")
+        if len(fname) < 10:
+            """
+            Skip 'README.md' (or other files which do not match the
+            naming convention).
+            """
+            continue
+        
+        data = np.loadtxt(fname = "generated_data/" + fnames[i], skiprows=1)
+        method = fname[1]
+        data_type = fname[8]
+        
+        if (filter_method != method) and (filter_method is not None):
+            """
+            Read only certain data files if filter_method is given as
+            input.
+            """
+            continue
+        
+        if (filter_data_type != data_type) and (filter_data_type is not None):
+            """
+            Read only certain data files if filter_data_type is given as
+            input.
+            """
+            continue
+        
+        n_particles = int(fname[2])
+        n_dims = int(fname[3])
+        n_mc_cycles = int(fname[4])
+        step_size = float(fname[5])
+        
+        if fname[6] == "numerical":
+            numerical = True
+        elif fname[6] == "analytical":
+            numerical = False
+        else:
+            msg = f"Expected 'numerical' or 'analytical'. Got '{fname[6]}'."
+            raise ValueError(msg)
+
+        if fname[7] == "interaction":
+            interaction = True
+        elif fname[7] == "nointeraction":
+            interaction = False
+        else:
+            msg = f"Expected 'interaction' or 'nointeraction'. Got '{fname[7]}'."
+            raise ValueError(msg)
+        
+        data_list.append(Container(
+            data,
+            method,
+            n_particles,
+            n_dims,
+            n_mc_cycles,
+            step_size,
+            numerical,
+            interaction,
+            data_type,
+            fnames[i]
+        ))
+
+    return data_list
+
 
 def get_number_particles(filename):
     """
@@ -12,8 +161,8 @@ def get_number_particles(filename):
 
 def read_from_file(filename):
     # n_particles = get_number_particles(filename)
-    alpha, var, exp = np.loadtxt(fname=filename, skiprows=2, unpack=True)
-    return alpha, var, exp#, n_particles
+    alpha, var, exp, time = np.loadtxt(fname=filename, skiprows=2, unpack=True)
+    return alpha, var, exp, time#, n_particles
 
 
 def read_energy_from_file_v1(filename, clip = False):
