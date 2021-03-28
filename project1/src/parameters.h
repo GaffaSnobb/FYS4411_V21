d54d7ea4f67fb27f435ef7e4ef0742f0672ba64c
Jon Dahl <jonkd@uio.no>
2021-03-28 22:33:13 +0200

    Add docstring.

--- project1/src/parameters.h
+++ project1/src/parameters.h
@@ -5,7 +5,7 @@ const double hbar = 1;
 const double m = 1;
 const double omega = 1;
 const double a = 0.0043;
-const double gamma_ = 2.82843;
+const double gamma_ = 2.82843;  // Shoud = beta.
 const double omega_z = gamma_*omega;
 
 #endif
