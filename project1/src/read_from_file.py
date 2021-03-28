c6fd39ad3d5830808c5ce6c9457346f6018cca32
Jon Dahl <jonkd@uio.no>
2021-03-28 19:13:58 +0200

    Add filter for all data file parameters.

--- project1/src/read_from_file.py
+++ project1/src/read_from_file.py
@@ -61,7 +61,16 @@ class Container:
         self.fname = fname
 
 
-def read_all_files(filter_method=None, filter_data_type=None):
+def read_all_files(
+    filter_method = None,
+    filter_n_particles = None,
+    filter_n_dims = None,
+    filter_n_mc_cycles = None,
+    filter_step_size = None,
+    filter_numerical = None,
+    filter_interaction = None,
+    filter_data_type = None
+):
     """
     Read all text files in generated_data/ and store all relevant data
     in 'Container' objects. See Container docstring for details.
@@ -73,6 +82,25 @@ def read_all_files(filter_method=None, filter_data_type=None):
         'brute', 'importance', 'gradient'. If None, no filter is
         applied.
 
+    filter_n_particles : NoneType, int
+        Filter for choosing a certain amount of particles.
+
+    filter_n_dims : NoneType, int
+        Filter for choosing a certain number of dimensions.
+
+    filter_n_mc_cycles : NoneType, int
+        Filter for choosing a certain number of Monte Carlo cycles.
+
+    filter_step_size : NoneType, float
+        Filter for choosing a certain brute force or importance step
+        size.
+
+    filter_numerical : NoneType, boolean
+        Filter for choosing numerical or analytical differentiation.
+
+    filter_interaction : NoneType, boolean
+        Filter for choosing interacting or non-interacting case.
+
     filter_data_type : NoneType, string
         Filter for only reading certain data types. Valid filters are
         'particle', 'onebody', 'energies'. If None, no filter is
@@ -83,6 +111,7 @@ def read_all_files(filter_method=None, filter_data_type=None):
     data_list : list
         List of Container objects.
     """
+
     fnames = os.listdir("generated_data/")
     data_list = []
 
@@ -97,9 +126,8 @@ def read_all_files(filter_method=None, filter_data_type=None):
             continue
         
         data = np.loadtxt(fname = "generated_data/" + fnames[i], skiprows=1)
+
         method = fname[1]
-        data_type = fname[8]
-        
         if (filter_method != method) and (filter_method is not None):
             """
             Read only certain data files if filter_method is given as
@@ -107,33 +135,79 @@ def read_all_files(filter_method=None, filter_data_type=None):
             """
             continue
         
+        n_particles = int(fname[2])
+        if (filter_n_particles != n_particles) and (filter_n_particles is not None):
+            """
+            Read only certain data files if filter_n_particles is given as
+            input.
+            """
+            continue
+
+        n_dims = int(fname[3])
+        if (filter_n_dims != n_dims) and (filter_n_dims is not None):
+            """
+            Read only certain data files if filter_n_dims is given as
+            input.
+            """
+            continue
+
+        n_mc_cycles = int(fname[4])
+        if (filter_n_mc_cycles != n_mc_cycles) and (filter_n_mc_cycles is not None):
+            """
+            Read only certain data files if filter_n_mc_cycles is given as
+            input.
+            """
+            continue
+
+        step_size = float(fname[5])
+        if (filter_step_size != step_size) and (filter_step_size is not None):
+            """
+            Read only certain data files if filter_step_size is given as
+            input.
+            """
+            continue
+
+        numerical = fname[6]
+        if (filter_numerical and (numerical != "numerical")):
+            continue
+
+        elif ((not filter_numerical) and (numerical != "analytical")):
+            continue
+
+        if fname[6] == "numerical":
+            numerical = True
+
+        elif fname[6] == "analytical":
+            numerical = False
+
+        else:
+            msg = f"Expected 'numerical' or 'analytical'. Got '{fname[6]}'."
+            raise ValueError(msg)
+
+        interaction = fname[7]
+        if (filter_interaction and (interaction != "interaction")):
+            continue
+
+        elif ((not filter_interaction) and (interaction != "nointeraction")):
+            continue
+
+        if fname[7] == "interaction":
+            interaction = True
+
+        elif fname[7] == "nointeraction":
+            interaction = False
+
+        else:
+            msg = f"Expected 'interaction' or 'nointeraction'. Got '{fname[7]}'."
+            raise ValueError(msg)
+
+        data_type = fname[8]
         if (filter_data_type != data_type) and (filter_data_type is not None):
             """
             Read only certain data files if filter_data_type is given as
             input.
             """
             continue
-        
-        n_particles = int(fname[2])
-        n_dims = int(fname[3])
-        n_mc_cycles = int(fname[4])
-        step_size = float(fname[5])
-        
-        if fname[6] == "numerical":
-            numerical = True
-        elif fname[6] == "analytical":
-            numerical = False
-        else:
-            msg = f"Expected 'numerical' or 'analytical'. Got '{fname[6]}'."
-            raise ValueError(msg)
-
-        if fname[7] == "interaction":
-            interaction = True
-        elif fname[7] == "nointeraction":
-            interaction = False
-        else:
-            msg = f"Expected 'interaction' or 'nointeraction'. Got '{fname[7]}'."
-            raise ValueError(msg)
         
         data_list.append(Container(
             data,
@@ -148,6 +222,10 @@ def read_all_files(filter_method=None, filter_data_type=None):
             fnames[i]
         ))
 
+    if not data_list:
+        msg = "No files have been loaded. Generate the data files or try another filter."
+        raise RuntimeError(msg)
+    
     return data_list
 
 
