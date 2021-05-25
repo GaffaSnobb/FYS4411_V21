01d043e76ed01e643566182af92a7b9c9bae8a92
Jon Dahl <jonkd@uio.no>
2021-05-25 18:57:43 +0200

    Compare generated data to M. Taut

--- project2/src/tune_omega.py
+++ project2/src/tune_omega.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from boltzmann_machine import ImportanceSampling, BruteForce
 import mplrc_params
 
-
 def parallel(arg_list: list):
     """
     This function is meant to be run in parallel with
@@ -19,7 +18,7 @@ def parallel(arg_list: list):
     
     q = ImportanceSampling(
         n_particles = 2,
-        n_dims = 2,
+        n_dims = 3,
         n_hidden = 2,
         n_mc_cycles = int(2**14),
         max_iterations = 50,
@@ -43,25 +42,37 @@ def parallel(arg_list: list):
 def main():
     fig, ax = plt.subplots()
 
-    # args = [[0, (0, 0)], [1, (0, 0.1)], [2, (0, 1)]]
-    omegas = np.arange(1, 6+1, 1)
-    args = [[proc, omega, np.sqrt(1/omega)] for proc, omega in enumerate(omegas)]
+    energies_taut = np.array([0.625, 0.175, 0.0822, 0.0477, 0.0311, 0.0219, 0.0162, 0.0125, 0.01, 0.0081])*2
+    omegas_taut_inverse = np.array([4, 20, 54.7386, 115.299, 208.803, 342.366, 523.102, 758.124, 1054.54, 1419.47])
+    omegas_taut = 1/omegas_taut_inverse
+    # omegas = np.arange(1, 6+1, 1)
+    
+    args = [[proc, omega, np.sqrt(1/omega)] for proc, omega in enumerate(omegas_taut)]
     pool = multiprocessing.Pool()
     res = pool.map(parallel, args)
 
-    energies = []
+    energies = np.zeros(len(res))
 
     for i in range(len(res)):
-        energies.append(res[i].energies[-1])
-        # ax.plot(range(res[i].max_iterations), res[i].energies, label="(loc, scale): " + f"{args[i][1]}")
+        energies[i] = res[i].energies[-1]
+    
+    for omega, our, taut, diff in zip(omegas_taut_inverse, energies, energies_taut, np.abs(energies - energies_taut)):
+        """
+        Print the values in LaTeX table format.
+        """
+        print(f"{omega} & {our:.4f} & {taut:.4f} & {diff:.4f}" + r" \\")
 
-    print(energies)
-    ax.plot(omegas, energies)
-    ax.set_xlabel(r"$\omega$")
+    ax.plot(1/omegas_taut, energies_taut, "--.", label="M. Taut")
+    ax.plot(1/omegas_taut, energies, "--.", label="This work")
+    # ax.set_title("3D")
+    ax.set_xlabel(r"1/$\omega$")
     ax.set_ylabel("Energy [a.u.]")
     ax.legend()
     ax.tick_params()
+    fig.savefig(fname="../fig/tune_omega_taut_vs_this_work.png", dpi=300)
     plt.show()
+
 
 if __name__ == "__main__":
     main()
+    pass
