d9155b234456ee2cbe2a2f9c797efc0d271c9be9
Jon Dahl <jon@dira.no>
2021-05-26 12:24:09 +0200

    Good parameters for heatmap production

--- project2/src/heatmap.py
+++ project2/src/heatmap.py
@@ -1,8 +1,10 @@
 import multiprocessing
+import time
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from boltzmann_machine import ImportanceSampling, BruteForce
+import mpl_rcparams
 
 def parallel(arg_list: list):
     """
@@ -14,33 +16,38 @@ def parallel(arg_list: list):
     arg_list:
         A list of arguments to pass to the class constructor.
     """
-    scale, max_iteration = arg_list
+    timing = time.time()
+    proc, scale, max_iteration = arg_list
+    omega = 1/4
+    sigma = np.sqrt(1/omega)
     
     q = ImportanceSampling(
         n_particles = 2,
-        n_dims = 2,
+        n_dims = 3,
+        # n_dims = 2,
         n_hidden = 2,
         n_mc_cycles = int(2**14),
         max_iterations = max_iteration,
         learning_rate = 0.05,
-        sigma = 1,              # Std. of the normal distribution the visible nodes.
+        sigma = sigma,
         interaction = True,
-        omega = 1,
+        omega = omega,
         diffusion_coeff = 0.5,
-        time_step = 0.05
+        time_step = 0.05,
+        parent_data_directory = (__file__.split(".")[0]).split("/")[-1]
     )
     q.initial_state(
         loc_scale_all = (0, scale)
     )
-    q.solve()
+    q.solve(verbose=False)
+
+    print(f"Process {proc} finished in {time.time() - timing:.3f}s with parameters {arg_list[1:]}")
 
     return q
 
 def main():
-
-    scales = [0.5, 1, 1.5, 2]
-    max_iterations = [20, 30, 40, 50]
-    max_iterations_reversed = list(reversed(max_iterations))
+    scales = [0, 1, 2, 3, 4]
+    max_iterations = [10, 20, 30, 40, 50, 60, 70]
     n_scales = len(scales)
     n_max_iterations = len(max_iterations)
 
@@ -49,36 +56,37 @@ def main():
     
     for i in range(n_scales):
         for j in range(n_max_iterations):
-            args.append([scales[i], max_iterations[j]])
+            args.append([i*n_max_iterations + j, scales[i], max_iterations[j]])
 
     pool = multiprocessing.Pool()
-    res = pool.map(parallel, args)
-    # np.random.shuffle(res)
-    
-    res.sort(key=lambda val: val.loc_scale_all[1]*1000 + val.max_iterations)
-
-    # for val in res:
-    #     print(val.max_iterations, val.loc_scale_all[1])
+    res = pool.map(parallel, args)    
+    res.sort(key=lambda val: val.loc_scale_all[1]*1000 + val.max_iterations)    # Trick to sort by both scale and max iterations
     
     for i in range(n_scales):
         for j in range(n_max_iterations):
-            grid[i, j] = res[i*n_scales + j].energies[-1]
+            grid[i, j] = res[i*n_max_iterations + j].energies[-1]
 
-    print(grid[-1, :])
 
-    # fig, ax = plt.subplots()
-    # ax = sns.heatmap(
-    #     data = grid,
-    #     linewidth = 0.5,
-    #     annot = True,
-    #     cmap = "viridis",
-    #     ax = ax,
-    #     xticklabels = scales,
-    #     yticklabels = max_iterations
-    # )
-    # # ax.invert_yaxis()
-    
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax = sns.heatmap(
+        data = grid,
+        linewidth = 0.5,
+        annot = True,
+        cmap = "viridis",
+        ax = ax,
+        yticklabels = scales,
+        xticklabels = max_iterations,
+        fmt = ".2f",
+        annot_kws = {"size": 15}
+    )
+    ax.tick_params(axis='y', rotation=0)
+    ax.set_xlabel(r"GD iterations")
+    ax.set_ylabel(r"Initial std")
+    cbar = ax.collections[0].colorbar
+    cbar.ax.set_ylabel(r"$E_L$ [a.u.]", rotation=90)
+    ax.invert_yaxis()
+    fig.savefig(fname="../fig/heatmap_iterations_init_scale.png", dpi=300)
+    plt.show()
 
 if __name__ == "__main__":
     main()
