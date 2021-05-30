ac064d6221987a96777328e84d474834f7500eba
Jon Dahl <jon@dira.no>
2021-05-30 10:53:36 +0200

    Add a few todos for rcparams

--- project2/src/mpl_rcparams.py
+++ project2/src/mpl_rcparams.py
@@ -10,3 +10,19 @@ mpl.rcParams['xtick.labelsize'] = 15
 mpl.rcParams['ytick.labelsize'] = 15
 mpl.rcParams['axes.titlesize'] = 15
 # mpl.rcParams['text.usetex'] = True
+
+
+# plt.rcParams.update({
+#     "text.usetex": True,
+#     "font.family": 'DeJavu Serif',
+#     "font.serif": "Computer Modern Roman",
+#     "font.sans-serif": ['Computer Modern Roman']})
+
+# plt.rc('font', size=16)  # controls default text size
+# plt.rc('axes', titlesize=16)  # fontsize of the title
+# plt.rc('axes', labelsize=16)  # fontsize of the x and y labels
+# plt.rc('xtick', labelsize=16)  # fontsize of the x tick labels
+# plt.rc('ytick', labelsize=16)  # fontsize of the y tick labels
+# plt.rc('legend', fontsize=24) #fontsize of the legend
+
+#\mathrm{}
