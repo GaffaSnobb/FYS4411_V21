94a58340eb8045ab57ca1609555bce5e67b87b42
Jon Dahl <jon@dira.no>
2021-05-30 10:54:33 +0200

    Parameter change

--- project2/src/variable_learning_rate.py
+++ project2/src/variable_learning_rate.py
@@ -15,7 +15,7 @@ def parallel(arg_list: list):
         A list of arguments to pass to the class constructor.
     """
     timing = time.time()
-    proc, max_iterations, learning_rate = arg_list
+    proc, learning_rate = arg_list
     omega = 1/4
     sigma = np.sqrt(1/omega)
     
@@ -23,18 +23,19 @@ def parallel(arg_list: list):
         n_particles = 2,
         n_dims = 3,
         n_hidden = 2,
-        n_mc_cycles = int(2**13),
-        max_iterations = max_iterations,
+        n_mc_cycles = int(2**14),
+        max_iterations = 50,
         learning_rate = learning_rate,
         sigma = sigma,
         interaction = True,
         omega = omega,
         diffusion_coeff = 0.5,
         time_step = 0.05,
-        parent_data_directory = (__file__.split(".")[0]).split("/")[-1]
+        parent_data_directory = (__file__.split(".")[0]).split("/")[-1],
+        rng_seed = 1337
     )
     q.initial_state(
-        loc_scale_all = (0, 1)
+        loc_scale_all = (0, 2)
     )
     q.solve(verbose=True)
 
@@ -45,18 +46,35 @@ def parallel(arg_list: list):
 def main():
     fig, ax = plt.subplots()
 
-    max_iterations = [50, 50]
-    learning_rates = [0.05, "variable"]
+    # learning_rates = [0.05, {"factor": 1, "t0": 2.5, "t1": 50}, {"factor": 2, "t0": 2.5, "t1": 50}, {"factor": 3, "t0": 2.5, "t1": 50}, {"factor": 4, "t0": 2.5, "t1": 50}]
+    learning_rate_init = 1
+
+    # learning_rates = [
+    #     learning_rate_init,
+    #     {"factor": 0.2, "init": learning_rate_init},
+    #     {"factor": 0.4, "init": learning_rate_init},
+    #     {"factor": 0.6, "init": learning_rate_init},
+    #     {"factor": 0.8, "init": learning_rate_init},
+    #     {"factor": 1.0, "init": learning_rate_init},
+    # ]
+    learning_rates = [
+        learning_rate_init,
+        {"factor": 0.005, "init": learning_rate_init},
+        {"factor": 0.010, "init": learning_rate_init},
+        {"factor": 0.015, "init": learning_rate_init},
+        {"factor": 0.020, "init": learning_rate_init},
+        {"factor": 0.025, "init": learning_rate_init},
+    ]
     args = []
 
-    for i in range(len(max_iterations)):
-        args.append([i, max_iterations[i], learning_rates[i]])
+    for i in range(len(learning_rates)):
+        args.append([i, learning_rates[i]])
 
     pool = multiprocessing.Pool()
     res = pool.map(parallel, args)
     
     for i in range(len(res)):
-        ax.plot(range(res[i].max_iterations), res[i].energies, label=r"$\eta$: " + f"{args[i][2]}")
+        ax.plot(range(res[i].max_iterations), res[i].energies, label=r"$\eta$: " + f"{args[i][1]}")
 
     ax.set_xlabel("Iterations")
     ax.set_ylabel("Energy")
