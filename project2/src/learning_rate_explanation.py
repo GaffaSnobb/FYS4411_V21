677abc320e9b7322c3051567a2be880bd69d63f5
Jon Dahl <jon@dira.no>
2021-05-31 18:16:20 +0200

    Produce plot suitable for the report

--- project2/src/learning_rate_explanation.py
+++ project2/src/learning_rate_explanation.py
@@ -1,18 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import other_functions as other
+import mpl_rcparams
 
 def main():
-    t = np.linspace(0, 50, 100)
-    t0 = np.arange(0, 6+1, 1)
-    t1 = np.arange(30, 100+5, 5)
+    iterations = np.arange(0, 50+1, 1)
+    factors = np.array([0.02, 0.06, 0.1])
+    N = len(factors)
 
-    for i in range(len(t1)):
-        # learning_rates = other.variable_learning_rate(t, t0[i], t1=75)
-        learning_rates = other.variable_learning_rate(t, t0=3, t1=t1[i])
-        plt.plot(t, learning_rates, label=f"{t1[i]=}")
-
-    plt.legend()
+    fig, ax = plt.subplots()
+    for i in range(N):
+        learning_rates = other.variable_learning_rate(
+            t = iterations*factors[i],
+            t0 = None,
+            t1 = None,
+            init = 0.18
+        )
+        ax.plot(iterations, learning_rates, label=r"$f_{\eta} = $" + f"{factors[i]}, " + r"$\eta_{init} = $" + f"{0.18}")
+    
+    ax.set_xlabel(r"GD iterations ($I$)")
+    ax.set_ylabel(r"$\eta$")
+    ax.legend()
+    fig.savefig(fname="../fig/variable_learning_rate_explanation.png", dpi=300)
     plt.show()
 
 if __name__ == "__main__":
