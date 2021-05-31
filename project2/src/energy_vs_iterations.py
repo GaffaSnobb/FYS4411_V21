92e1b260e36c5c185d0a17c25d4d89bfc7ba1c4d
Jon Dahl <jon@dira.no>
2021-05-31 18:15:09 +0200

    Cleanup, parameter change, docstring update

--- project2/src/energy_vs_iterations.py
+++ project2/src/energy_vs_iterations.py
@@ -50,9 +50,13 @@ def parallel(arg_list: list):
     return q
 
 def main():
+    """
+    Plot local energy as a function of gradient descent iterations for
+    different learning rates. Include variable learning rate.
+    """
     # learning_rates = [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2]
     # learning_rates = [0.02, 0.06, 0.1, 0.14, 0.18]
-    learning_rates = [0.02, 0.1, 0.18]
+    learning_rates = [0.02, 0.1, 0.18, {"factor": 0.05, "init": 0.18}]
     n_learning_rates = len(learning_rates)
     # scales = [0.5, 1, 1.5, 2]
     scales = [0.5, 1.5]
@@ -70,39 +74,25 @@ def main():
 
     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8, 7))
     ax = ax.ravel()
-    # axins = ax.inset_axes([0.5, 0.5, 0.47, 0.47])
     proc = 0
     for i in range(n_scales):
-        ax[i].hlines(y=0.5, xmin=0, xmax=100, label=r"Exact ($E_L = 0.5$)", linestyle="dashed", color="black")
+        ax[i].hlines(y=0.5, xmin=0, xmax=100, label=r"Exact ($E_L = 0.5$ a.u.)", linestyle="dashed", color="black")
         for j in range(n_learning_rates):
-            ax[i].plot(range(res[proc].max_iterations), res[proc].energies, label=r"Learning rate: " + f"{learning_rates[j]}")
+            label = r"$\eta$ = " + f"{learning_rates[j]}"
+            if isinstance(learning_rates[j], dict):
+                label = r"$\eta_{init} = $" + f"{learning_rates[j]['init']}"
+                label += r" $f_{\eta} = $" + f"{learning_rates[j]['factor']}"
+            ax[i].plot(range(res[proc].max_iterations), res[proc].energies, label=label)
             proc += 1
-        # axins.plot(range(res[i].max_iterations), res[i].energies)
-        # ax.plot(res[i].max_iterations, res[i].energies[-1], "o")
 
     ax[0].set_xticklabels("")
     ax[0].legend(fontsize=12)
     ax[1].set_xlabel("GD iterations")
-    ax[0].set_title(f"Initial STD: 0.5")
-    ax[1].set_title(f"Initial STD: 1.5")
+    ax[0].set_title(r"$\sigma_{init} = 0.5$")
+    ax[1].set_title(r"$\sigma_{init} = 1.5$")
     fig.text(s=r"$E_L$ [a.u.]", x=0.02, y=0.45, rotation=90, fontsize=15)
     fig.savefig(fname="../fig/energy_vs_iterations_learning_rates.png", dpi=300)
     plt.show()
-
-    # ax.plot([10], [10]) # Force matplotlob to label y = 10
-    # ax.set_yscale("log")
-    # ax.legend(fontsize=12, loc="upper left")
-    # axins.set_yscale("log")
-    # axins.hlines(y=0.5, xmin=0, xmax=100, label=r"Exact ($E_L = 0.5$)", linestyle="dashed", color="black")
-
-    # axins.set_xticklabels("")
-    # axins.set_yticklabels("")
-
-    # x1, x2, y1, y2 = 80, 100, 0.48, 0.54
-    # axins.set_xlim(x1, x2)
-    # axins.set_ylim(y1, y2)
-    # ax.indicate_inset_zoom(axins, edgecolor="black")
-
 
 if __name__ == "__main__":
     main()
