0eafdd502e386f6b9b5c4ca028073f34ae56b710
Jon Dahl <jon@dira.no>
2021-06-01 17:37:53 +0200

    Implement variation of scale, update docstring

--- project2/src/reproduce_analytical_non_interacting.py
+++ project2/src/reproduce_analytical_non_interacting.py
@@ -15,7 +15,7 @@ def parallel(arg_list: list):
         A list of arguments to pass to the class constructor.
     """
     timing = time.time()
-    proc, n_particles, n_dims = arg_list
+    proc, n_particles, n_dims, scale = arg_list
     omega = 1
     sigma = np.sqrt(1/omega)
     
@@ -36,7 +36,7 @@ def parallel(arg_list: list):
         parent_data_directory = (__file__.split(".")[0]).split("/")[-1]
     )
     q.initial_state(
-        loc_scale_all = (0, 0.5)
+        loc_scale_all = (0, scale)
     )
     q.solve(verbose=False)
 
@@ -45,15 +45,21 @@ def parallel(arg_list: list):
     return q
 
 def main():
+    """
+    Reproduce analytical results for 1, 2, 3 dimensions with 1 and 2
+    particles. For 0 and 0.5 initial scale (std).
+    """
     n_particles = [1, 2]
     n_dims = [1, 2, 3]
+    scales = [0, 0.5]
     args = []
 
     proc = 0
-    for n_particle in n_particles:
-        for n_dim in n_dims:
-            args.append([proc, n_particle, n_dim])
-            proc += 1
+    for i in range(len(n_particles)):
+        for k in range(len(scales)):
+            for j in range(len(n_dims)):
+                args.append([proc, n_particles[i], n_dims[j], scales[k]])
+                proc += 1
 
     pool = multiprocessing.Pool()
     results = pool.map(parallel, args)
@@ -67,9 +73,9 @@ def main():
     results_exact = ["1/2", "1", "3/2", "1", "2", "3"]
 
     for i in range(len(n_particles)):
-        print(f"{i+1}, 1 & \({results[i*3].energies[-1]:.3f} \pm {results_blocking[i*3]:.1g}\) & {results_exact[i*3]}" + r" \\")
-        print(f"{i+1}, 2 & \({results[i*3 + 1].energies[-1]:.3f} \pm {results_blocking[i*3 + 1]:.1g}\) & {results_exact[i*3 + 1]}" + r" \\")
-        print(f"{i+1}, 3 & \({results[i*3 + 2].energies[-1]:.3f} \pm {results_blocking[i*3 + 2]:.1g}\) & {results_exact[i*3 + 2]}" + r" \\")
+        print(f"{i+1}, 1 & \({results[i*6 + 0].energies[-1]:.3f} \pm {results_blocking[i*6 + 0]:5.1g}\) & \({results[i*6 + 3].energies[-1]:.3f} \pm {results_blocking[i*6 + 3]:5.1g}\) & {results_exact[i*3]:4s}" + r" \\")
+        print(f"{i+1}, 2 & \({results[i*6 + 1].energies[-1]:.3f} \pm {results_blocking[i*6 + 1]:5.1g}\) & \({results[i*6 + 4].energies[-1]:.3f} \pm {results_blocking[i*6 + 4]:5.1g}\) & {results_exact[i*3 + 1]:4s}" + r" \\")
+        print(f"{i+1}, 3 & \({results[i*6 + 2].energies[-1]:.3f} \pm {results_blocking[i*6 + 2]:5.1g}\) & \({results[i*6 + 5].energies[-1]:.3f} \pm {results_blocking[i*6 + 5]:5.1g}\) & {results_exact[i*3 + 2]:4s}" + r" \\")
 
 if __name__ == "__main__":
     main()
