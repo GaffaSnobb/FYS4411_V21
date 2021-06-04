fbf9e2c76af59f0b0bd71d272e6266fc69bfa093
Jon Dahl <jon@dira.no>
2021-06-04 22:18:19 +0200

    Parameter change

--- project2/src/energy_vs_iterations.py
+++ project2/src/energy_vs_iterations.py
@@ -2,7 +2,7 @@ import time
 import multiprocessing
 import matplotlib.pyplot as plt
 import numpy as np
-from boltzmann_machine import ImportanceSampling
+from boltzmann_machine import BruteForce, ImportanceSampling
 import mpl_rcparams
 
 def parallel(arg_list: list):
@@ -20,31 +20,30 @@ def parallel(arg_list: list):
     omega = 1
     sigma = np.sqrt(1/omega)
     
-    q = ImportanceSampling(
+    q = BruteForce(
         n_particles = 1,
         n_dims = 1,
         n_hidden = 2,
-        n_mc_cycles = int(2**15),
-        max_iterations = 100,
+        n_mc_cycles = int(2**18),
+        max_iterations = 12,
         learning_rate = learning_rate,
         sigma = sigma,
         interaction = False,
         omega = omega,
-        diffusion_coeff = 0.5,
-        time_step = 0.05,
+        brute_force_step_size = 1,
         parent_data_directory = (__file__.split(".")[0]).split("/")[-1],
-        rng_seed = 1337
+        rng_seed = 1337+4
         # rng_seed = seed
     )
     q.initial_state(
         loc_scale_all = (0, scale)
-    )
-    if proc == 0:
-        verbose = True
-    else:
-        verbose = False
+    )    
     
-    q.solve(verbose)
+    q.solve(
+        verbose = True if proc == 0 else False,
+        save_state = True,
+        load_state = True
+    )
 
     print(f"Process {proc} finished in {time.time() - timing:.3f}s with parameters {arg_list[1:]}")
 
@@ -55,15 +54,14 @@ def main():
     Plot local energy as a function of gradient descent iterations for
     different learning rates. Include variable learning rate.
     """
-    # learning_rates = [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2]
-    # learning_rates = [0.02, 0.06, 0.1, 0.14, 0.18]
-    learning_rates = [0.02, 0.1, 0.18, {"factor": 0.05, "init": 0.18}]
-    # learning_rates = [0.1, 0.18, {"factor": 0.05, "init": 0.18}]
+    learning_rates = [0.5, {"factor": 0.05, "init": 0.5}, 0.6, {"factor": 0.05, "init": 0.6}]
+    # learning_rates = [0.05, {"factor": 0.5, "init": 0.05}, 0.1, {"factor": 0.5, "init": 0.1}]
+    scales = [0.5, 4]
+    # learning_rates = [0.2, {"factor": 0.05, "init": 0.2}, 0.25, {"factor": 0.05, "init": 0.25}]#, 0.3, {"factor": 0.05, "init": 0.3}]
+    # scales = [3, 4]
     n_learning_rates = len(learning_rates)
-    # scales = [0.5, 1, 1.5, 2]
-    scales = [0.5, 1.5]
     n_scales = len(scales)
-    seed = int(time.time())  # Same seed for each proc, but different between runs
+    seed = np.random.randint(100000)
     
     args = []
     proc = 0
@@ -79,20 +77,23 @@ def main():
     ax = ax.ravel()
     proc = 0
     for i in range(n_scales):
-        ax[i].hlines(y=0.5, xmin=0, xmax=100, label=r"Exact ($E_L = 0.5$ a.u.)", linestyle="dashed", color="black")
+        ax[i].hlines(y=0.5, xmin=0, xmax=12, label=r"Exact ($E_L = 0.5$ a.u.)", linestyle="dashed", color="black")
+        # ax[i].set_yscale("log")
         for j in range(n_learning_rates):
             label = r"$\eta$ = " + f"{learning_rates[j]}"
+            linestyle = "solid"
             if isinstance(learning_rates[j], dict):
                 label = r"$\eta_{init} = $" + f"{learning_rates[j]['init']}"
                 label += r" $f_{\eta} = $" + f"{learning_rates[j]['factor']}"
-            ax[i].plot(range(res[proc].max_iterations), res[proc].energies, label=label)
+                linestyle = "dashed"
+            ax[i].plot(range(res[proc].max_iterations), res[proc].energies, label=label, linestyle=linestyle)
             proc += 1
 
     ax[0].set_xticklabels("")
     ax[0].legend(fontsize=12)
     ax[1].set_xlabel("GD iterations")
-    ax[0].set_title(r"$\sigma_{init} = 0.5$")
-    ax[1].set_title(r"$\sigma_{init} = 1.5$")
+    ax[0].set_title(r"$\sigma_{init} = $" + f"{res[0].loc_scale_all[1]}")
+    ax[1].set_title(r"$\sigma_{init} = $" + f"{res[-1].loc_scale_all[1]}")
     fig.text(s=r"$E_L$ [a.u.]", x=0.02, y=0.45, rotation=90, fontsize=15)
     fig.savefig(fname="../fig/energy_vs_iterations_learning_rates.png", dpi=300)
     plt.show()
