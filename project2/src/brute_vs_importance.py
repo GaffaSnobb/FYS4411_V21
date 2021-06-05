df611d1dcc9d8412fa4921d4a5e4ea651c9f5325
Jon Dahl <jon@dira.no>
2021-06-05 12:12:22 +0200

    Final state of the program

--- project2/src/brute_vs_importance.py
+++ project2/src/brute_vs_importance.py
@@ -1,6 +1,5 @@
 import multiprocessing
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 from boltzmann_machine import ImportanceSampling, BruteForce
 
@@ -22,7 +21,6 @@ def parallel(arg_list: list):
     n_particles = 1
     n_dims = 1
     n_hidden = 2
-    # n_mc_cycles = int(2**14)
     max_iterations = 50
     learning_rate = 0.05
     interaction = True
@@ -76,55 +74,47 @@ def parallel(arg_list: list):
     return q
 
 def main():
-    # args = [
-    #     [0, "brute", int(2**14)],
-    #     [2, "brute", int(2**13)],
-    #     [3, "brute", int(2**12)],
-    #     [4, "brute", int(2**11)],
-    #     [5, "brute", int(2**10)],
-    #     [6, "importance", int(2**14)],
-    #     [7, "importance", int(2**13)],
-    #     [8, "importance", int(2**12)],
-    #     [9, "importance", int(2**11)],
-    #     [1, "importance", int(2**10)]
-    # ]
+    """
+    Produce comparison data of brute force and importance sampling.
+    Print the results in a LaTeX formatted table.
+    """
     args = [
         ["brute", int(2**18)],
-        ["brute", int(2**17)],
-        ["brute", int(2**16)],
-        ["brute", int(2**15)],
-        ["brute", int(2**14)],
-        ["brute", int(2**13)],
-        ["brute", int(2**12)],
-        ["brute", int(2**11)],
-        ["brute", int(2**10)],
         ["importance", int(2**18)],
+        ["brute", int(2**17)],
         ["importance", int(2**17)],
+        ["brute", int(2**16)],
         ["importance", int(2**16)],
+        ["brute", int(2**15)],
         ["importance", int(2**15)],
+        ["brute", int(2**14)],
         ["importance", int(2**14)],
+        ["brute", int(2**13)],
         ["importance", int(2**13)],
+        ["brute", int(2**12)],
         ["importance", int(2**12)],
+        ["brute", int(2**11)],
         ["importance", int(2**11)],
+        ["brute", int(2**10)],
         ["importance", int(2**10)],
     ]
 
     args = [[proc, method, mc] for proc, [method, mc] in enumerate(args)]
-    exact = 0.5
-    tol = 0.01
+    exact = 0.5 # Exact energy of the system [a.u.]
+    tol = 0.01  # Tolerance where the result is good enough [a.u.]
     pool = multiprocessing.Pool()
     results = pool.map(parallel, args)
     n_results = len(results)
     goal_indices = np.zeros(n_results, dtype=int)
 
-    iterations = range(results[0].max_iterations)
-    
-    fig, ax = plt.subplots()
-
     for i in range(n_results):
         goal_indices[i] = np.where(np.abs(results[i].energies - exact) < tol)[0][0]
 
     for i in range(n_results):
+        """
+        Print results from the iteration where the tolerance is
+        achieved.
+        """
         method = results[i].__str__().split()[0]
         n_mc_cycles = results[i].n_mc_cycles
         energy = results[i].energies[goal_indices[i]]
@@ -135,26 +125,16 @@ def main():
 
     print()
     for i in range(n_results):
+        """
+        Print results from the final iteration.
+        """
         method = results[i].__str__().split()[0]
         n_mc_cycles = results[i].n_mc_cycles
         energy = results[i].energies[-1]
-        diff = np.abs(energy - exact)
         energy_std = results[i].blocking_all[-1]
         timing = np.sum(results[i].times[:-1])
         gd_iterations_end = results[i].max_iterations
         print(f"{method:11s} & {n_mc_cycles:6d} & \({energy:7.4f} \pm {energy_std:7.1g}\) & {(np.abs(energy - exact))*100/exact:7.4f} & {gd_iterations_end} & {timing:.1f}" + r" \\")
-    # for i in range(5, len(results)):
-    #     ax.plot(iterations, results[i].energies, label=results[i].n_mc_cycles)
-    #     ax.plot(iterations[goal_indices[i]], results[i].energies[goal_indices[i]], "ro")
-    #     ax.fill_between(
-    #         x = iterations,
-    #         y1 = results[i].energies - results[i].blocking_all,
-    #         y2 = results[i].energies + results[i].blocking_all,
-    #         color = "black",
-    #         alpha = 0.2,
-    #     )
-    # ax.legend()
-    # plt.show()
 
 if __name__ == "__main__":
     main()
