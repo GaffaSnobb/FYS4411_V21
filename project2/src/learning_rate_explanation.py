d2ff6ae982ad2fe08ea9702ad20bce23c678f7d3
Jon Dahl <jon@dira.no>
2021-06-06 18:35:57 +0200

    Update docstring, clean up code

--- project2/src/learning_rate_explanation.py
+++ project2/src/learning_rate_explanation.py
@@ -4,6 +4,9 @@ import other_functions as other
 import mpl_rcparams
 
 def main():
+    """
+    Produce a plot to show how the variable learning rate evolves.
+    """
     iterations = np.arange(0, 50+1, 1)
     factors = np.array([0.02, 0.06, 0.1])
     N = len(factors)
@@ -16,7 +19,11 @@ def main():
             t1 = None,
             init = 0.18
         )
-        ax.plot(iterations, learning_rates, label=r"$f_{\eta} = $" + f"{factors[i]}, " + r"$\eta_{init} = $" + f"{0.18}")
+        ax.plot(
+            iterations,
+            learning_rates,
+            label = r"$f_{\eta} = $" + f"{factors[i]}, " + r"$\eta_{init} = $" + f"{0.18}"
+        )
     
     ax.set_xlabel(r"GD iterations ($I$)")
     ax.set_ylabel(r"$\eta$")
